52d3bdcf23f56a6edd2dbc433c56753a63cf523d
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-03-26 01:42:14 +0600

    first commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='bg-danger'>Cup Of Tea</h1>
+            <h4>Select Four Items</h4>
+        </div>
+    );
+};
+
+export default Header;
