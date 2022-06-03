4840948d933f0cc462a97411545b5d0dba64567c
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-06-04 00:00:46 +0600

    headlines changed

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='headers'>
             <h1 className='text-center'>Book Store</h1>
-            <h4 className='text-center'>Select Four Items</h4>
+            <h4 className='text-center'>Select Any Four Items</h4>
         </div>
     );
 };
